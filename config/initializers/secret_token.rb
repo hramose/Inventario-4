e0d8f6af5ab552adbac7274a21789ad972076271
viewor <contact@viewor.com.mx>
2012-03-16 13:09:19 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventario::Application.config.secret_token = 'd578bfa963bdc210f5304727e45d011c34a0137a35cbcb04b47ac81d6e3cc20cdcf003fe8aff7a0302e7ca3f8fe046fe48236e090fbd2eedd9cb1d13f931377b'
